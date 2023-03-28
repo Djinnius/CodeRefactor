02e1c256c1ab07fff5a383f40475ccb7a6d0ccea
Jonathan Asquith <jonathan_d_asquith@hotmail.com>
2023-03-28 22:43:44 +0100

    Added non static project

---
+++ src/NonStatic/Controllers/SpeedLimitController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NonStatic.Services;
+using LazyCache;
+
+namespace Monolith.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpeedLimitController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetSpeedLimitInKilometersPerHour()
+        {
+            var appcache = new CachingService();
+            var coordinateProvider = new CoordinateProvider(appcache);
+            var coordinateAggregator = new CoordinateAggregator(coordinateProvider);
+            var speedLimitCalculator = new SpeedLimitCalculator(coordinateAggregator);
+            var speedLimit = speedLimitCalculator.GetSpeedLimitForCurrentPosition();
+            return speedLimit.Match(m => Ok(m), _ => Ok("No Speed Limit"));
+        }
+    }
+}
